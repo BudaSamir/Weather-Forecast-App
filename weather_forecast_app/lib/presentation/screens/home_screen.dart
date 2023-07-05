25ac1419039956e74e4a56a4208bf35d286e2911
Buda Samir <Abdulrahman.samir.attia.elseed.96@gmail.com>
2023-07-05 22:08:54 +0300

    Initialize weather variables

--- weather_forecast_app/lib/presentation/screens/home_screen.dart
+++ weather_forecast_app/lib/presentation/screens/home_screen.dart
@@ -8,6 +8,17 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
+  //Initialization
+  int temperature = 0;
+  int maxTemp = 0;
+  String weatherStateName = 'Loading.....';
+  int humidity = 0;
+  int windSpeed = 0;
+  var currentDate = 'Loading...';
+  String imageUrl = '';
+  // where on earth id
+  int woeid = 44418; // London is the default city
+  String Location = 'London';
   @override
   Widget build(BuildContext context) {
     return Scaffold(
